29481dbc75a82d212c68a9fa11efb8b5352330a2
anjali7528 <anjaligandhi248@gmail.com>
2022-03-16 22:13:49 +0530

    new commit

--- src/components/AddNewItem.js
+++ src/components/AddNewItem.js
@@ -1,25 +1,40 @@
 import React from 'react';
 
 
-class AddNewItem extends React.Component(){
+//class AddNewItem extends React.Component(){
 
-  handleChange =(e) => {
-    this.setState({
-      newitem : e.target.value
-    });
-  }
+function AddNewItem(){
+  // constructor(props){
+  //   super(props);
+  //   this.state={
+  //     newitem: ' ',
+  //     newprice: 0
+  //   }
+  // }
 
-  render(){
+  // handleChangeItem =(e) => {
+  //   this.setState({
+  //     newitem : e.target.value
+  //   });
+  // }
+
+  // handleChangePrice =(e) => {
+  //   this.setState({
+  //     newprice : e.target.value
+  //   });
+  // }
+
+  // render(){
   return (
     <div><h2>AddNewItem</h2>
     <form>
-        Item Name: <input type="text" placeholder='Add Item ' onChange = {this.handleChange}></input>
-        Item Price:<input type="text" placeholder='Add price'></input>
-        <button className='btn'> Add Item</button>
+        Item Name: <input type="text" placeholder='Add Item ' /*onChange = {this.handleChangeItem}*/></input>
+        Item Price:<input type="text" placeholder='Add price' /*onChange = {this.handleChangePrice}*/></input>
+        <button className='btn' onClick={() => this.handleAddNewItem()}> Add Item</button>
     </form>
     </div>
   )
 }
-}
+//}
 
 export default AddNewItem;
